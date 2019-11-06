1bf48888b179eb4257a18353c7ba2ed222de808d
Shaun Krueger <ShaunK@iqmetrix.com>
2019-11-06 13:05:11 -0600

    Assignment Changes

--- 1 - DataTypes/Code/WeirdCalculator.cs
+++ 1 - DataTypes/Code/WeirdCalculator.cs
@@ -6,25 +6,28 @@ namespace DataTypes.Code
 	{
 		public static long SimpleWholeNumberAdd(int firstValue, long secondValue)
 		{
-			throw new NotImplementedException();
-		}
+            var result = (long)firstValue+secondValue;
+            return result;
+            throw new NotImplementedException();
+        }
 
 		public static decimal SimpleDecimalAdd(float firstValue, double secondValue, decimal thirdValue)
 		{
-			/*
+            /*
 			 * Decimals, Floats and Doubles can be complex (the long comment in `ComplexDecimalAdd` below describes why but read that later).
 			 * 
 			 * HOWEVER - for this test, don't worry about the complex nature of them, assume all values passed in *CAN* be converted to decimals!
 			 * 
 			 * Hint: You can make this method's body one line!
 			 */
-
+            var result = (decimal)firstValue+(decimal)secondValue+thirdValue;
+            return result;
 			throw new NotImplementedException();
 		}
 
-		public static decimal ComplexDecimalAdd(float firstValue, double secondValue, decimal thirdValue)
-		{
-			/*
+        public static decimal ComplexDecimalAdd(float firstValue, double secondValue, decimal thirdValue)
+        {
+            /*
 			 * There's an interesting thing to point out when comparing double/float and decimal:
 			 * The range of double is ±5.0 × 10−324 to ±1.7 × 10308, while the range of decimal is (-7.9 x 1028 to 7.9 x 1028) / (100 to 28).
 			 * The range of float is half of double.
@@ -39,23 +42,34 @@ namespace DataTypes.Code
 			 * I will give you one clue / tip - You *will* need to handle exceptions for the final solution.
 			 * If you are new to exceptions, google "C# Exception Handling" - litterally half our jobs is googling :P
 			 */
-
-			throw new NotImplementedException();
+            decimal result=0;
+            try
+            {
+               result = (decimal)firstValue + (decimal)secondValue + thirdValue;
+            }
+            catch (OverflowException)
+            {
+                 result = 0;
+            }
+            return result;
+            
+            throw new NotImplementedException();
 		}
 
 		public static decimal ComplexAdd(char firstValue, string secondValue)
 		{
-			/*
+            /*
 			 * You will notice the TDD tests around this just "assume" it will work. Do not worry about handling exceptions or errors, assume input is valid.
 			 */
-
+            var result = decimal.Parse(char.ToString(firstValue))+decimal.Parse(secondValue); //this seems messy but since ToDecimal(Char) is not supported I couldn't think of any other way
+            return result;
 			throw new NotImplementedException();
 		}
 
 		public static decimal ConditionalMath(int firstValue, int secondValue, bool returnNegative)
 		{
 			var actual = firstValue + secondValue;
-
+            actual = Math.Abs(actual);
 			if (returnNegative)
 			{
 				actual = actual * -1;
